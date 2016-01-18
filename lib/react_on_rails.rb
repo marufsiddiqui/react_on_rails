c6a658977c57cb3f85fe6975253d6357d8fb60e9
Rob Wise <robert.wise@outlook.com>
2016-01-18 18:02:41 -0500

    Fix requires
    Put require statements all in one place. This will hopefully
    address an issue where the user is warned that React on Rails
    is attempting to set a constant twice. This is happening because
    the version.rb file is being required from multiple locations.

--- lib/react_on_rails.rb
+++ lib/react_on_rails.rb
@@ -1,6 +1,8 @@
 require "rails"
 
 require "react_on_rails/version"
+require "react_on_rails/version_checker"
 require "react_on_rails/configuration"
 require "react_on_rails/server_rendering_pool"
 require "react_on_rails/engine"
+require "raect_on_rails/version_syntax_converter"
